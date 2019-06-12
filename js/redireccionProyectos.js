930368ecab90f7f8922dfa43253ace3cd9151827
rodrigogomez14 <rodrigogomez548@gmail.com>
2019-06-12 16:07:06 -0300

    0.2.1 (section websites)

--- js/redireccionProyectos.js
+++ js/redireccionProyectos.js
@@ -6,3 +6,7 @@ const $rYm = document.getElementById('rick-morty')
 $rYm.addEventListener("click", () => {
     window.location.href = 'https://rodrigogomez14.github.io/Rick-Morty/'
 })
+const $VG = document.getElementById('valvulasGomez')
+$VG.addEventListener("click", () => {
+    window.location.href = 'http://www.valvulasgomez.com'
+})
