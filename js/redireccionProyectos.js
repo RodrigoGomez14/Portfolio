56b2050489e9a8286aecdb6469afc856b7d4eb06
rodrigogomez14 <rodrigogomez548@gmail.com>
2019-06-14 00:55:29 -0300

    Platzi video agregada

--- js/redireccionProyectos.js
+++ js/redireccionProyectos.js
@@ -6,7 +6,13 @@ const $rYm = document.getElementById('rick-morty')
 $rYm.addEventListener("click", () => {
     window.location.href = 'https://rodrigogomez14.github.io/Rick-Morty/'
 })
+const $PV = document.getElementById('platziVideo')
+$PV.addEventListener("click", () => {
+    window.location.href = 'https://rodrigogomez14.github.io/PlatziVideo/'
+})
 const $VG = document.getElementById('valvulasGomez')
 $VG.addEventListener("click", () => {
     window.location.href = 'http://www.valvulasgomez.com'
 })
+
+
